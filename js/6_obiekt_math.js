703ec7c77df4d3d8cb251910da0e6f1e884a0d6d
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 11:56:31 +0200

    conditions

--- js/6_obiekt_math.js
+++ js/6_obiekt_math.js
@@ -1,1 +1,10 @@
 'use strict'
+
+/*
+obiekt Math
+*/
+
+console.log(Math.PI);
+
+console.log(Math.cos(0));
+
---
+++ js/7_warunki.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var wzrostMateusz = 190;
+var wzrostOlgi = 180;
+
+/*
+
+warunek if
+
+
+*/
+
+if (wzrostOlgi < wzrostMateusz) {
+    
+    console.log("Olga jest niższa :)");
+}
+
+
+/*
+
+Warunek if else
+
+*/
+
+
+if (wzrostOlgi > wzrostMateusz) {
+    
+    console.log("Olga jest wyższa :)");
+}else {
+    console.log("Olga jest niższa");
+    
+}
+    
+/*
+
+warunek if else if
+*/
+
+if (wzrostOlgi > wzrostMateusz) {
+    console.log("Olga jest wyższa");
+
+}else if(wzrostOlgi == wzrostMateusz){
+    
+    console.log("Olga tak wysoka jak Mateusz");
+    
+}else {
+    
+    console.log("Olga jest niższa");
+    
+}
+
+/*
+switch
+*/
+
+var kolor = "zielony";
+
+switch(kolor) {
+        
+    case "czerwony":
+        console.log("Kolor czerwony")
+        break;
+    case "zielony":
+        console.log("Kolor zielony")
+        break;
+    case "niebieski":
+        console.log("Kolor niebieski")
+        break;
+    default:
+        console.log("Inny kolor")
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
