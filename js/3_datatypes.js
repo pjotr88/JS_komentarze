1b52afa4e41263c4638c154391d13976f598eb3d
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 10:44:06 +0200

    Data types

---
+++ js/3_datatypes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+var wyplataString = "5000";
+var premiaString = "2500";
+var calkowitaWyplataString;
+
+calkowitaWyplataString = wyplataString + premiaString;
+
+console.log(calkowitaWyplataString);
+
+/*zmienna boolean*/
+
+var czyJestSmog;
+
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log("JestSmog");
+}else{
+    console.log("Nie ma Smogu");
+}
+
+/* 
+
+typy specjalne - undefined / null
+
+*/
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+
+var imieStudenta = "Michal";
+console.log(imieStudenta.indexOf('c'));
