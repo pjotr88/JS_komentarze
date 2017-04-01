c9c6192241385accb4ca33345e286c9572a6bab7
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 14:49:25 +0200

    funkcje

---
+++ js/10_funkcje.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function multiply(parametr1, parametr2, parametr3) {
+    
+    var result;
+    
+    result = parametr1 * parametr2 * parametr3;
+    
+    return result;
+    
+}
+
+var wynik = multiply(3, 9, 10);
+
+//console.log("wynik: " + wynik);
+
+var funkcja = function () {
+    return "wynik :)"
+};
+
+console.log(funkcja);
+console.log(funkcja());
