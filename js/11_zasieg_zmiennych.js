d4b7557cb923ebe009cdf733b890c4112dbb473b
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 15:28:40 +0200

    zasięg zmiennych

---
+++ js/11_zasieg_zmiennych.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var liczba1 = 3, liczba2 = 5, liczba3 =40;
+
+
+
+function multiply(parametr1, parametr2, parametr3) {
+    
+    var result;
+    
+    result = parametr1 * parametr2 * parametr3;
+    
+    return result;
+    
+}
+
+var wynik = multiply(liczba1, liczba2, liczba3);
+
+console.log(wynik);
