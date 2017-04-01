48a6a9875d98ee3b89e62008e3feb6c53e990ca4
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 11:18:02 +0200

    zmienne_obiektowe2

---
+++ js/5_zmienne_obiektowe.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+obiekty jako zmienne
+*/
+
+var student = {
+    imie: "Kaja",
+    wzrost: 152,
+    wyswietlDane: function() {console.log(this.imie)}
+
+}
+
+student.wyswietlDane();
+
+console.log(student);
