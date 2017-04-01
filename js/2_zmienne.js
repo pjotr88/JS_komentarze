650b5d9dfd98c51ddc49ee1fa7e5b3d70fa1230e
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-01 09:39:37 +0200

    zmienne

---
+++ js/2_zmienne.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+var kolor ="czerwony";
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
+
+
+wiek = 21;
+
+
+console.log(wiek);
