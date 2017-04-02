353200c03fde105e2a154b1de4b7a66840b157ea
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-02 10:08:21 +0200

    json cwiczenie

---
+++ js/15_cwiczenie_Json.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var jsonPracownicy = {
+    pracownicy: [
+        {firstName: "Krystian", lastName: "Dziopa"},
+        {firstName: "Anna", lastName: "Szapiel"},
+        {firstName: "Piotr", lastName: "Żmuda"},
+        
+    ] 
+    
+};
+
+
+console.log(jsonPracownicy);
+
+jsonPracownicy.pracownicy.forEach(function (element, index) 
+                        { 
+    
+console.log(index + " " + element.firstName + " " + element.lastName);
+                         
+
+                        })
+                
